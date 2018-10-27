bfb2bc9e73cfa2e5e0150514aadbed63c68da40d
Philippe Matray <phmatray@outlook.com>
2018-10-27 16:13:07 +0200

    Code Splitting with React Router v4

--- src/routes/CustomRoutes.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import routes from './routes';
-import CustomRoute from './CustomRoute';
-
-const CustomRoutes = () => routes.map(route => <CustomRoute key={route.path} {...route} />);
-
-export default CustomRoutes;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,146 +1,162 @@
-import Dashboard from '../components/dashboard/Dashboard';
-import CreateProfile from '../components/create-profile/CreateProfile';
-import EditProfile from '../components/edit-profile/EditProfile';
-import Profile from '../components/profile/Profile';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Loadable from 'react-loadable';
+import AuthenticatedRoute from './AuthenticatedRoute';
+import Loading from './Loading';
 
-import Login from '../pages/Login';
-import Register from '../pages/Register';
-import Landing from '../pages/Landing';
-import Share from '../pages/Share';
-import Jammers from '../pages/Jammers';
-import Playlist from '../pages/Playlist';
-import Playlists from '../pages/Playlists';
-import Track from '../pages/Track';
-import Tracks from '../pages/Tracks';
-import Artist from '../pages/Artist';
-import Artists from '../pages/Artists';
-import Albums from '../pages/Albums';
-import Album from '../pages/Album';
-import Labels from '../pages/Labels';
-import Label from '../pages/Label';
-import Post from '../pages/Post/Post';
+const AsyncLogin = Loadable({
+  loader: () => import('../pages/Login'),
+  loading: Loading,
+  timeout: 10000,
+});
 
-const routes = [
-  {
-    path: '/',
-    exact: true,
-    isPrivate: false,
-    main: Landing,
-  },
-  {
-    path: '/register',
-    exact: true,
-    isPrivate: false,
-    main: Register,
-  },
-  {
-    path: '/login',
-    exact: true,
-    isPrivate: false,
-    main: Login,
-  },
-  {
-    path: '/jammers',
-    exact: true,
-    isPrivate: false,
-    main: Jammers,
-  },
-  {
-    path: '/profile/:handle',
-    exact: true,
-    isPrivate: false,
-    main: Profile,
-  },
-  {
-    path: '/playlists',
-    exact: true,
-    isPrivate: false,
-    main: Playlists,
-  },
-  {
-    path: '/playlist/:id',
-    exact: true,
-    isPrivate: false,
-    main: Playlist,
-  },
-  {
-    path: '/tracks',
-    exact: true,
-    isPrivate: false,
-    main: Tracks,
-  },
-  {
-    path: '/track/:id',
-    exact: true,
-    isPrivate: false,
-    main: Track,
-  },
-  {
-    path: '/artists',
-    exact: true,
-    isPrivate: false,
-    main: Artists,
-  },
-  {
-    path: '/artist/:id',
-    exact: true,
-    isPrivate: false,
-    main: Artist,
-  },
-  {
-    path: '/albums',
-    exact: true,
-    isPrivate: false,
-    main: Albums,
-  },
-  {
-    path: '/album/:id',
-    exact: true,
-    isPrivate: false,
-    main: Album,
-  },
-  {
-    path: '/labels',
-    exact: true,
-    isPrivate: false,
-    main: Labels,
-  },
-  {
-    path: '/label/:id',
-    exact: true,
-    isPrivate: false,
-    main: Label,
-  },
-  {
-    path: '/share',
-    exact: true,
-    isPrivate: false,
-    main: Share,
-  },
-  {
-    path: '/dashboard',
-    exact: true,
-    isPrivate: true,
-    main: Dashboard,
-  },
-  {
-    path: '/create-profile',
-    exact: true,
-    isPrivate: true,
-    main: CreateProfile,
-  },
-  {
-    path: '/edit-profile',
-    exact: true,
-    isPrivate: true,
-    main: EditProfile,
-  },
-  {
-    path: '/post/:id',
-    exact: true,
-    isPrivate: true,
-    main: Post,
-  },
-];
+const AsyncRegister = Loadable({
+  loader: () => import('../pages/Register'),
+  loading: Loading,
+  timeout: 10000,
+});
 
-export default routes;
+const AsyncLanding = Loadable({
+  loader: () => import('../pages/Landing'),
+  loading: Loading,
+  timeout: 10000,
+});
+
+const AsyncShare = Loadable({
+  loader: () => import('../pages/Share'),
+  loading: Loading,
+  timeout: 10000,
+});
+
+const AsyncJammers = Loadable({
+  loader: () => import('../pages/Jammers'),
+  loading: Loading,
+  timeout: 10000,
+});
+
+const AsyncProfile = Loadable({
+  loader: () => import('../components/profile/Profile'),
+  loading: Loading,
+  timeout: 10000,
+});
+
+const AsyncPlaylists = Loadable({
+  loader: () => import('../pages/Playlists'),
+  loading: Loading,
+  timeout: 10000,
+});
+
+const AsyncPlaylist = Loadable({
+  loader: () => import('../pages/Playlist'),
+  loading: Loading,
+  timeout: 10000,
+});
+
+const AsyncTracks = Loadable({
+  loader: () => import('../pages/Tracks'),
+  loading: Loading,
+  timeout: 10000,
+});
+
+const AsyncTrack = Loadable({
+  loader: () => import('../pages/Track'),
+  loading: Loading,
+  timeout: 10000,
+});
+
+const AsyncArtists = Loadable({
+  loader: () => import('../pages/Artists'),
+  loading: Loading,
+  timeout: 10000,
+});
+
+const AsyncArtist = Loadable({
+  loader: () => import('../pages/Artist'),
+  loading: Loading,
+  timeout: 10000,
+});
+
+const AsyncAlbums = Loadable({
+  loader: () => import('../pages/Albums'),
+  loading: Loading,
+  timeout: 10000,
+});
+
+const AsyncAlbum = Loadable({
+  loader: () => import('../pages/Album'),
+  loading: Loading,
+  timeout: 10000,
+});
+
+const AsyncLabels = Loadable({
+  loader: () => import('../pages/Labels'),
+  loading: Loading,
+  timeout: 10000,
+});
+
+const AsyncLabel = Loadable({
+  loader: () => import('../pages/Label'),
+  loading: Loading,
+  timeout: 10000,
+});
+
+const AsyncDashboard = Loadable({
+  loader: () => import('../components/dashboard/Dashboard'),
+  loading: Loading,
+  timeout: 10000,
+});
+
+const AsyncCreateProfile = Loadable({
+  loader: () => import('../components/create-profile/CreateProfile'),
+  loading: Loading,
+  timeout: 10000,
+});
+
+const AsyncEditProfile = Loadable({
+  loader: () => import('../components/edit-profile/EditProfile'),
+  loading: Loading,
+  timeout: 10000,
+});
+
+const AsyncNotFound = Loadable({
+  loader: () => import('../components/not-found/NotFound'),
+  loading: Loading,
+  timeout: 10000,
+});
+
+export default ({ childProps }) => (
+  <Switch>
+    <Route path="/" exact component={AsyncLanding} props={childProps} />
+    <Route path="/login" exact component={AsyncLogin} props={childProps} />
+    <Route path="/register" exact component={AsyncRegister} props={childProps} />
+    <Route path="/share" exact component={AsyncShare} props={childProps} />
+    <Route path="/jammers" exact component={AsyncJammers} props={childProps} />
+    <Route path="/profile/:handle" exact component={AsyncProfile} props={childProps} />
+    <Route path="/playlists" exact component={AsyncPlaylists} props={childProps} />
+    <Route path="/playlist/:id" exact component={AsyncPlaylist} props={childProps} />
+    <Route path="/tracks" exact component={AsyncTracks} props={childProps} />
+    <Route path="/track/:id" exact component={AsyncTrack} props={childProps} />
+    <Route path="/artists" exact component={AsyncArtists} props={childProps} />
+    <Route path="/artist/:id" exact component={AsyncArtist} props={childProps} />
+    <Route path="/albums" exact component={AsyncAlbums} props={childProps} />
+    <Route path="/album/:id" exact component={AsyncAlbum} props={childProps} />
+    <Route path="/labels" exact component={AsyncLabels} props={childProps} />
+    <Route path="/label/:id" exact component={AsyncLabel} props={childProps} />
+    <AuthenticatedRoute path="/dashboard" exact component={AsyncDashboard} props={childProps} />
+    <AuthenticatedRoute
+      path="/create-profile"
+      exact
+      component={AsyncCreateProfile}
+      props={childProps}
+    />
+    <AuthenticatedRoute
+      path="/edit-profile"
+      exact
+      component={AsyncEditProfile}
+      props={childProps}
+    />
+
+    {/* Finally, catch all unmatched routes */}
+    <Route component={AsyncNotFound} />
+  </Switch>
+);
