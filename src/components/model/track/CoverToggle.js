44a2fd095bce90bf1c92a13737dc9250de7b0a1d
Philippe Matray <phmatray@outlook.com>
2018-11-06 00:25:03 +0100

    Add a play button when a user hover a track item

--- src/components/model/track/CoverToggle.js
+++ src/components/model/track/CoverToggle.js
@@ -3,17 +3,17 @@ import PropTypes from 'prop-types';
 
 import { Wrapper, Cover, Overlay, Icon } from './cover-toggle/Atoms';
 
-const CoverToggle = ({ playSelected, pause, playlist, track, isActive, playerPlaying }) => {
+const CoverToggle = ({ playTrack, pause, playlist, track, isActive, isNew, playerPlaying }) => {
   const showPause = playerPlaying && isActive;
 
   return (
     <Wrapper>
       <Cover
         src={track.coverurl.w200}
-        style={{ width: '3em', height: '3em', borderRadius: '5%' }}
         alt={track.title}
+        label={isNew && { corner: 'left', icon: 'time', size: 'mini', color: 'teal' }}
       />
-      <Overlay onClick={() => (showPause ? pause() : playSelected(playlist, track))}>
+      <Overlay onClick={() => (showPause ? pause() : playTrack(track))}>
         <Icon
           name={`${showPause ? 'pause' : 'play'} circle outline`}
           inverted
@@ -27,7 +27,7 @@ const CoverToggle = ({ playSelected, pause, playlist, track, isActive, playerPla
 };
 
 CoverToggle.propTypes = {
-  playSelected: PropTypes.func.isRequired,
+  playTrack: PropTypes.func.isRequired,
   pause: PropTypes.func.isRequired,
   playlist: PropTypes.object.isRequired,
   track: PropTypes.shape({
@@ -38,7 +38,12 @@ CoverToggle.propTypes = {
     }).isRequired,
   }).isRequired,
   isActive: PropTypes.bool.isRequired,
+  isNew: PropTypes.bool,
   playerPlaying: PropTypes.bool.isRequired,
+};
+
+CoverToggle.defaultProps = {
+  isNew: true,
 };
 
 export default CoverToggle;
