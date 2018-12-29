2d261f717d2556553a9f66d8c6881282d88dea7b
Philippe Matray <phmatray@gmail.com>
2018-12-29 18:08:32 +0100

    Linting fix

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  extends: [
+    'airbnb',
+    'plugin:flowtype/recommended',
+    'plugin:prettier/recommended',
+    'plugin:jsx-a11y/recommended',
+  ],
+  env: {
+    browser: true,
+    jest: true,
+  },
+  parser: 'babel-eslint',
+  plugins: ['flowtype', 'prettier', 'jsx-a11y'],
+  rules: {
+    'no-console': 'off',
+    'no-shadow': 0,
+    'no-underscore-dangle': ['error', { allow: ['_id', '__REDUX_DEVTOOLS_EXTENSION__'] }],
+    'prefer-destructuring': ['error', { object: true, array: false }],
+    'import/no-extraneous-dependencies': [
+      'error',
+      { devDependencies: ['.storybook/**', '**/stories.js', '**/*.test.js'] },
+    ],
+    'jsx-a11y/label-has-associated-control': 'off',
+    'jsx-a11y/label-has-for': 'off',
+    'react/destructuring-assignment': [false],
+    'react/forbid-prop-types': [false],
+    'react/jsx-filename-extension': [1, { extensions: ['.js'] }],
+    'react/jsx-no-bind': [false],
+    'react/no-array-index-key': [false],
+    'react/prop-types': [false],
+  },
+};
--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -8,7 +8,7 @@ class ErrorBoundary extends Component {
 
   static getDerivedStateFromError(error) {
     // Update state so the next render will show the fallback UI.
-    return { hasError: true };
+    return { hasError: error && true };
   }
 
   componentDidCatch(error, info) {
